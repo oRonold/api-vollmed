e518f491cfc47246faa6bbf2375c3131b82c154b
oRonold <or040918@gmail.com>
2024-02-21 17:01:56 -0300

    Endpoint POST medicos finalizado, conexão com o banco de dados feita com a migration FlyWay.

---
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -0,0 +1,11 @@
+package med.voll.api.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.swing.text.html.parser.Entity;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+
+
+
+}
