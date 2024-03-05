71a46878cb48f191a9e8bcc4e3acb52ed35d52dc
oRonold <or040918@gmail.com>
2024-03-05 10:43:11 -0300

    Desabilitando cross site refresh forge do Spring Securiy

--- src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/exception/TratadorDeErros.java
@@ -1,7 +1,6 @@
-package med.voll.api.infra;
+package med.voll.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
-import org.apache.tomcat.util.http.parser.AcceptLanguage;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
